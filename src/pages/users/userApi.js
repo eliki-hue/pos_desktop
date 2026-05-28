2e8ebe7dfb2398e7a74c0341d949d260b1b8f87d
eliki <eliki13720@gmail.com>
2026-05-28 23:29:58 +0300

    debug deactivate user in admin page

--- src/pages/users/userApi.js
+++ src/pages/users/userApi.js
@@ -4,5 +4,5 @@ export const userApi = {
   list: () => api.get("/api/users/"),
   create: (data) => api.post("/api/users/", data),
   update: (id, data) => api.patch(`/api/users/${id}/`, data),
-  deactivate: (id) => api.post(`/api/users/${id}/deactivate/`),
+  deactivate: (id) => api.patch(`/api/users/${id}/deactivate/`),
 };
