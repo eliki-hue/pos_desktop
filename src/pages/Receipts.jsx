ddcc3968f25448dcbe26142385af1ce20356745b
eliki <eliki13720@gmail.com>
2026-05-28 21:36:55 +0300

    increased receipt font size

--- src/pages/Receipts.jsx
+++ src/pages/Receipts.jsx
@@ -49,7 +49,7 @@ function ReceiptModal({ receipt, onClose }) {
               top: 0;
               width: 80mm;
               font-family: 'Arial', 'Helvetica', 'Courier New', monospace;
-              font-size: 15pt !important;
+              font-size: 17pt !important;
               line-height: 1.2 !important;
             }
             
@@ -65,7 +65,7 @@ function ReceiptModal({ receipt, onClose }) {
             
             .print-area table {
               width: 100%;
-              font-size: 15pt !important;
+              font-size: 17pt !important;
             }
             
             .print-area th, .print-area td {
